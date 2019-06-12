af39f4d8410a3b27f1e175e85ec86a02c0a17d5e
Anwesh43 <anweshthecool0@gmail.com>
2019-06-12 10:22:49 +0530

    adding boxupdoubleview module

--- boxupdoubleview/src/main/java/com/anwesh/uiprojects/boxupdoubleview/BoxUpDoubleView.kt
+++ boxupdoubleview/src/main/java/com/anwesh/uiprojects/boxupdoubleview/BoxUpDoubleView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.boxupdoubleview
 /**
  * Created by anweshmishra on 12/06/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4A148C")
+val backColor : Int = Color.parseColor("#BDBDBD")
